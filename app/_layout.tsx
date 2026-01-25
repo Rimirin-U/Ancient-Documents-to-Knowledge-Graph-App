660faf2a1c607ebd338275147b64912033f4ad0d
Rimirin-U <T202410486994650@eduxiji.net>
2026-01-25 23:12:26 +0800

    Hello

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,14 @@
+import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
+import { useColorScheme } from '@/hooks/use-color-scheme';
 import { Stack } from "expo-router";
 
 export default function RootLayout() {
-  return <Stack />;
+  const colorScheme = useColorScheme();
+  return (
+    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+      <Stack>
+         <Stack.Screen name='index' options={{ headerShown : false }} />
+      </Stack>
+    </ThemeProvider>
+  );
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,18 @@
-import { Text, View } from "react-native";
+import { ThemedText } from "@/app-example/components/themed-text";
+import { ThemedView } from "@/app-example/components/themed-view";
 
 export default function Index() {
   return (
-    <View
+    <ThemedView
       style={{
         flex: 1,
         justifyContent: "center",
         alignItems: "center",
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
+      <ThemedText>
+        Hello!
+      </ThemedText>
+    </ThemedView>
   );
 }
